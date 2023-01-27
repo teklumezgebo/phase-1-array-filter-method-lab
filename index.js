8c5af041b9254ca5671393cc4124cde1f6bb3d93
Teklu Mezgebo <mezge002@umn.edu>
2023-01-26 19:37:30 -0600

    commit tree 9e57597c74258bc1aa03d6842a6a5149e2a609d3

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(list) {
+    let newList = []
+    for (const item of list) {
+        if (item === item) {
+            newList.push(item)
+        }
+    }
+
+    return newList
+
+}
